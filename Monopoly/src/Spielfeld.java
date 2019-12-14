a94749bc7d162fddff406cf0efb4a65d2d74c1af
kenoe <kenoe@192.168.0.16>
2019-12-14 17:46:44 +0100

    Revert "CHANGE: wuerfle methode ADD: config properties"
    This reverts commit 151a617a780baf5ad1c9173e2938f95096e953bd.

--- Monopoly/src/Spielfeld.java
+++ Monopoly/src/Spielfeld.java
@@ -3,8 +3,6 @@ public class Spielfeld {
 	private Feld[] felder;
 
 	public static void main(String[] args) {
-		Spielfigur spieler1 = new Spielfigur();
-		System.out.println(spieler1.wuerfeln());
-		System.out.println(spieler1.getPasch());
+
 	}
 }
