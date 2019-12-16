988213ada8fda076b3a498a0987169d038a7143e
Takrem1 <jan.rabenstein1@web.de>
2019-12-16 10:51:27 +0100

    CHANGE: Test

--- Monopoly/src/Gemeinschaftsfeld.java
+++ Monopoly/src/Gemeinschaftsfeld.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Gemeinschaftsfeld extends Feld {
 
 	public Gemeinschaftsfeld() {
+		System.out.println("123");
 	}
 
 	public void betrittFeld(Spielfigur spieler, ArrayList<Spielfigur> nichtspieler, Feld[] felder) {
