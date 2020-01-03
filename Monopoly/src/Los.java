e0f3460a871684db94d1f8cfdae97cf0853c2737
ChristinaKicki <christina-kickingereder@freenet.de>
2020-01-03 13:32:22 +0100

    TEst Los

--- Monopoly/src/Los.java
+++ Monopoly/src/Los.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 
 public class Los extends Feld {
 
-	private int betrag;
+	private int betrag = 400; // 4000 oder 400?
 
 	public Los() {
 	}
 
-	public void betrittFeld(Spielfigur spieler, ArrayList<Spielfigur> nichtspieler, Feld[] felder) {
-		System.out.println ("Test2");
+	public void betrittFeld(Spielfigur spieler, ArrayList<Spielfigur> nichtspieler, Feld[] felder, int betrag) {
+		System.out.println (spieler.getName + " hat das Fel'Los' betreten und erhält 400 €");
+		Spielfigur.getGeld(Geld + betrag);
 	}
 }
