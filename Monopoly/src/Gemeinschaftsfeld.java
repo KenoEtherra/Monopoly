ea331187df98d907f0101a1707f80e2bf4c2e3e0
Takrem1 <jan.rabenstein1@web.de>
2020-01-04 20:35:05 +0100

    Ereignisfeld mit switch case

--- Monopoly/src/Gemeinschaftsfeld.java
+++ Monopoly/src/Gemeinschaftsfeld.java
@@ -1,11 +1,15 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class Gemeinschaftsfeld extends Feld {
-
+	
 	public Gemeinschaftsfeld() {
-		System.out.println("1234445757");
+		
 	}
-
+	 
 	public void betrittFeld(Spielfigur spieler, ArrayList<Spielfigur> nichtspieler, Feld[] felder) {
 		// TODO Auto-generated method stub
 
