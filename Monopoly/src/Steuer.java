b1ff573166e8cdcaa8997784601e2dc2f3367d6b
Christina <Christina@Christina-PC>
2020-01-05 15:07:33 +0100

    test steuertopf

--- Monopoly/src/Steuer.java
+++ Monopoly/src/Steuer.java
@@ -4,6 +4,8 @@ public class Steuer extends Feld {
 
 	private int betrag1 = 200;
 	private int betrag2 = 100;
+	
+	private int steuertopf = 0;
 
 	public Steuer() {
 		super();
@@ -12,12 +14,22 @@ public class Steuer extends Feld {
 	public void betrittFeld(Spielfigur spieler, ArrayList<Spielfigur> nichtspieler, Feld[] felder) {
 		if (spieler.getPosition() == 4) {
 			spieler.setGeld (spieler.getGeld() - betrag1);
+			setSteuertopf (getSteuertopf() + betrag1);
 			System.out.println(spieler.getName() + "muss 200 € Einkommenssteuer zahlen.");
 		}
 		if (spieler.getPosition() == 38) {
 			spieler.setGeld (spieler.getGeld() - betrag2);
+			setSteuertopf (getSteuertopf() + betrag2);
 			System.out.println(spieler.getName() + "muss 100 € Zusatzsteuer zahlen.");
 		}
 
+	}
+
+	public int getSteuertopf() {
+		return steuertopf;
+	}
+
+	public void setSteuertopf(int steuertopf) {
+		this.steuertopf = steuertopf;
 	}
 }
