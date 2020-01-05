3254806b5e76f6c22469138b9f7862c9164c9830
MarasMarslion <larsbruns94@web.de>
2020-01-05 11:42:40 +0100

    Erste Änderung am Spielfeld

--- Monopoly/src/Spielfeld.java
+++ Monopoly/src/Spielfeld.java
@@ -17,6 +17,24 @@ public class Spielfeld extends Main {
         //38 Super Tax
         //2,17,33 Gemeinschaft
         //7,22,36 Ereignis
+    	Los los = new Los();
+    	//Strasse(int Serie, int Strasse, String Name, String Farbe)
+    	Strasse badstrasse = new Strasse(0,0,"Badstrasse","lila");
+    	Strasse turmstrasse = new Strasse(0,1,"Turmstrasse","lila");
+    	Strasse chausseestrasse = new Strasse(1,0,"Chausseestrasse","hellblau");
+    	Strasse elisenstrasse = new Strasse(1,1,"Elisenstrasse","hellblau");
+    	Strasse poststrasse = new Strasse(1,2,"Poststrasse","hellblau");
+    	Strasse seestrasse = new Strasse(2,0,"Seestrasse","pink");
+    	Strasse hafenstrasse = new Strasse(2,1,"Hafenstrasse","pink");
+    	Strasse neuestrasse = new Strasse(2,2,"Neue Strasse","pink");
+    	Strasse müchenerstrasse = new Strasse(3,0,"Münchener Strasse","orange");
+    	Strasse wienerstrasse = new Strasse(3,1,"Wiener Strasse","orange");
+    	Strasse berlinerstrasse = new Strasse(3,3,"Berliner Strasse","orange");
+    	Strasse theaterstrasse = new Strasse(4,0,"Theaterstrasse","rot");
+    	
+    	
+    	
+    	felder[0]=los;
     }
 
     public void starteSpiel() {
