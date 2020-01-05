084e4d48a38b68d72b75a483090073f6d1945ceb
Onek42 <joe1keno@yahoo.de>
2020-01-05 15:19:30 +0100

    ADD: Freiparken

---
+++ Monopoly/src/Freiparken.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Freiparken extends Feld {
+
+	public Freiparken() {
+		super();
+	}
+
+	public void betrittFeld(Spielfigur spieler, ArrayList<Spielfigur> nichtspieler, Feld[] felder) {
+
+	}
+
+}
